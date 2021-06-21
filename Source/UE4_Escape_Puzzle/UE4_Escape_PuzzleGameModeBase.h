58fa9eb02a6a4ef69ae09a45443ae300ee830389
victordoyle <victor-doyle@outlook.com>
2021-06-20 20:36:41 -0400

    base commit

---
+++ Source/UE4_Escape_Puzzle/UE4_Escape_PuzzleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4_Escape_PuzzleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_ESCAPE_PUZZLE_API AUE4_Escape_PuzzleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
